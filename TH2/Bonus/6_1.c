cba8bb259656fb855da5ec414ef378f274995a01
Duc Huy (Hui) <48280785+huyluongme@users.noreply.github.com>
2022-05-01 11:17:15 +0700

    Create 6_1.c

---
+++ TH2/Bonus/6_1.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#define MAX 100
+
+typedef struct {
+	int u, v;
+}Edge;
+
+typedef struct {
+	int n, m;
+	Edge edges[MAX];
+}Graph;
+
+typedef struct {
+	int data[MAX];
+	int front, rear;
+}queue;
+
+void make_null_queue(queue* pQ) {
+	pQ->front = 0;
+	pQ->rear = -1;
+}
+
+void enqueue(queue* pQ, int u) {
+	pQ->rear++;
+	pQ->data[pQ->rear] = u;
+}
+
+void dequeue(queue* pQ) {
+	pQ->front++;
+}
+
+int front(queue Q) {
+	return Q.data[Q.front];
+}
+
+int empty(queue Q) {
+	return Q.front > Q.rear;
+}
+
+void init_graph(Graph* pG, int n) {
+	pG->n = n;
+	pG->m = 0;
+}
+
+void add_edge(Graph* pG, int u, int v) {
+	pG->edges[pG->m].u = u;
+	pG->edges[pG->m].v = v;
+	pG->m++;
+}
+
+int adjacent(Graph* pG, int u, int v) {
+	for (int e = 0; e < pG->m; e++)
+		if ((pG->edges[e].u == u && pG->edges[e].v == v)
+			|| (pG->edges[e].u == v && pG->edges[e].v == u))
+			return 1;
+	return 0;
+}
+
+int mark[MAX];
+
+void BFS(Graph* pG, int u) {
+	queue Q;
+	make_null_queue(&Q);
+	enqueue(&Q, u);
+	while (!empty(Q)) {
+		int s = front(Q);
+		dequeue(&Q);
+		if (mark[s] != 0)
+			continue;
+
+		printf("%d\n", s);
+		mark[s] = 1;
+
+		for (int v = 1; v <= pG->n; v++)
+			if (adjacent(pG, s, v))
+				enqueue(&Q, v);
+	}
+}
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	Graph G;
+	init_graph(&G, n);
+	for (int e = 1; e <= m; e++) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		add_edge(&G, u, v);
+	}
+
+	for (int u = 1; u <= G.n; u++)
+		mark[u] = 0;
+	for (int u = 1; u <= G.n; u++)
+		if (!mark[u])
+			BFS(&G, u);
+	return 0;
+}
