2f00e5a643e4be0a7879f6ae662f72e5e0c797bc
Duc Huy (Hui) <48280785+huyluongme@users.noreply.github.com>
2022-05-14 17:13:33 +0700

    Create 6_1.c

---
+++ TH3/6_1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+typedef struct {
+	int u, v, w;
+}Edge;
+
+typedef struct {
+	int n, m;
+	Edge edges[100];
+}Graph;
+
+void init_graph(Graph* pG, int n) {
+	pG->n = n;
+	pG->m = 0;
+}
+
+void add_edge(Graph* pG, int u, int v, int w) {
+	pG->edges[pG->m].u = u;
+	pG->edges[pG->m].v = v;
+	pG->edges[pG->m].w = w;
+	pG->m++;
+}
+
+#define oo 999999
+int mark[100], p[100], pi[100];
+
+void BellmanFord(Graph* pG, int s) {
+	for (int u = 1; u <= pG->n; u++) {
+		mark[u] = 0;
+		pi[u] = oo;
+	}
+	pi[s] = 0;
+	p[s] = -1;
+
+	for (int i = 1; i < pG->n; i++) {
+		for (int e = 0; e < pG->m; e++) {
+			int u = pG->edges[e].u;
+			int v = pG->edges[e].v;
+			int w = pG->edges[e].w;
+			if (pi[u] + w < pi[v]) {
+				pi[v] = pi[u] + w;
+				p[v] = u;
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	scanf("%d%d", &n, &m);
+	Graph G;
+	init_graph(&G, n);
+	for (int e = 0; e < m; e++) {
+		int u, v, w;
+		scanf("%d %d %d", &u, &v, &w);
+		add_edge(&G, u, v, w);
+	}
+	BellmanFord(&G, 1);
+	if (pi[n] < oo)
+		printf("%d", pi[n]);
+	else
+		printf("-1");
+	return 0;
+}
